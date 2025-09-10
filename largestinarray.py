09f84122972b71f6d6c681794668c6e9501b1ba6
Sreeparvathy Rajeev <pinkparvathy@gmail.com>
2025-09-10 20:43:52 +0530

    modifications

--- largestinarray.py
+++ largestinarray.py
@@ -1,12 +1,14 @@
-#largest element in array
+
 list1=[0]*6
 for i in range(6):  
     list1[i]=int(input("enter the number"))
 print(list1)
+
+#optimal method to find largest element
 def largest():
-    l=0
+    l=list1[0]
     for i in range(6):
-        if list1[i]>list1[l]:
-            l=i
-    print("largest element is",list1[l])
+        if list1[i]>l:
+            l=list1[i]
+    print("largest element is",l)
 largest()
