9d3c0b90e1ab24394323abd2ef564f7afa2129ec
Sreeparvathy Rajeev <pinkparvathy@gmail.com>
2025-10-01 13:27:09 +0530

    update

---
+++ ARRAY/binary_search/binarysearch.py
@@ -0,0 +1,14 @@
+#arr is a sorted array
+#returns the index of target if found else returns -1
+def binarysearch(arr,target):
+    left = 0
+    right = len(arr)-1
+    while left<=right:
+        mid=(left+right)//2
+        if arr[mid]==target:
+            return mid
+        elif arr[mid]<target:
+            left=mid+1
+        else:
+            right=mid-1
+    return -1
